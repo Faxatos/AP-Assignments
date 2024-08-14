bd95ed8daa891e39225f107326effb8df015f372
Faxy <emilianosescu@gmail.com>
2024-08-14 18:31:15 +0200

    ended class constructor istances, defined required operations
    Assignment1Ex3 push

--- Assignment 1/Exercise 3/MultiSet.hs
+++ Assignment 1/Exercise 3/MultiSet.hs
@@ -13,30 +13,110 @@ module MultiSet (
 data MSet a = MS [(a, Int)]
     deriving (Show)
 
+---------------
 -- Constructors
+---------------
 
 -- empty :: MSet a
 -- Returns an empty MSet
 empty :: MSet a
 empty = MS []
 
+------------------------------
 -- Class Constructor Instances
+------------------------------
 
--- Eq instance for MSet type: two multisets are equal if they contain the same elements with the same multiplicity, regardless of the order
+-- Eq instance for MSet using the subset method
 instance Eq a => Eq (MSet a) where
-    -- The two multisets are equal if their sorted version are equal
-    MS mset1 == MS mset2 = sortedMSet mset1 == sortedMSet mset2
-        where
-            sortedMSet = sortMSet
+    -- The two multisets are equal if both are "strict" subset of each other 
+    -- (meaning that the multiplicity of the elements must be the same)
+    MS mset1 == MS mset2 = strictSubeq (MS mset1) (MS mset2) && strictSubeq (MS mset2) (MS mset1)
 
--- Util functions
+-- Foldable instance for MSet (using foldr to create the minimal implementation for Foldable)
+instance Foldable MSet where
+    foldr f acc (MS []) = acc
+    foldr f acc (MS ((x, n):xs)) = foldr f (foldr f acc (MS xs)) (replicate n x)
 
--- Sorting function (insertion sort) that sorts based on the first element of the tuple
-sortMSet :: Ord a => [(a, Int)] -> [(a, Int)]
-sortMSet [] = []
-sortMSet (x:xs) = insert x (sortMSet xs)
+{-
+Explanation for not implementing Functor:
+
+The MSet type can't be made an instance of Functor because it would break the Functor laws. 
+Specifically, fmap is supposed to apply a function to each element independently, without altering the structure of the functor. 
+However, in an MSet, if fmap (see the mapMSet implementation in Util functions section) mapped two distinct elements to the same result, 
+we would need to combine their multiplicities to keep the MSet well-formed. 
+This changes the structure, which isn't allowed by Functor laws. 
+The structure should stay the same, and only the values should be modified by fmap 
+(“The structure of the functor remains unchanged and only the values are modified.” - https://wiki.haskell.org/Functor#Functor_Laws). 
+This means that MSet doesn’t fit the requirements for Functor.
+-}
+
+-------------
+-- Operations
+-------------
+
+-- add an element to the MSet, increasing its multiplicity if it already exists
+add :: Eq a => MSet a -> a -> MSet a
+add (MS mset) v = MS (addHelper mset v)
   where
-    insert y [] = [y]
-    insert y (z:zs)
-        | fst y <= fst z = y : z : zs
-        | otherwise      = z : insert y zs
+    -- function to add element to a multiset
+    addHelper [] v = [(v, 1)]
+    addHelper ((x, n):xs) v
+        | x == v    = (x, n+1) : xs
+        | otherwise = (x, n) : addHelper xs v
+
+-- return the number of occurrences of an element in the MSet
+occs :: Eq a => MSet a -> a -> Int
+occs (MS mset) v = case lookup v mset of
+    Just n  -> n
+    Nothing -> 0
+
+-- return a list containing all the elements of the MSet
+elems :: MSet a -> [a]
+elems (MS mset) = concatMap replicateElem mset
+  where
+    -- function to expand the MSet type to all the occurences of the element in the "bag"
+    replicateElem (x, n) = replicate n x
+
+-- check if the first MSet is a subset of the second MSet
+subeq :: Eq a => MSet a -> MSet a -> Bool
+subeq (MS mset1) (MS mset2) = all checkElem mset1
+  where
+    -- function to check if an element in mset1 has at least the same multiplicity in mset2
+    checkElem (x, n) = occs (MS mset2) x >= n
+
+-- return a new MSet with the union of the two input MSets
+union :: Eq a => MSet a -> MSet a -> MSet a
+union (MS mset1) (MS mset2) = MS (unionMSet mset1 mset2)
+  where
+    -- function to combine elements from the second MSet into the first MSet
+    unionMSet :: Eq a => [(a, Int)] -> [(a, Int)] -> [(a, Int)]
+    unionMSet mset1 [] = mset1
+    unionMSet mset1 ((x, n):xs) =
+        unionMSet (addOrUpdate x n mset1) xs
+
+    -- function to add or update an element in the MSet
+    addOrUpdate :: Eq a => a -> Int -> [(a, Int)] -> [(a, Int)]
+    addOrUpdate x n [] = [(x, n)]
+    addOrUpdate x n ((y, m):ys)
+        | x == y    = (y, m + n) : ys
+        | otherwise = (y, m) : addOrUpdate x n ys
+
+-----------------
+-- Util functions
+-----------------
+
+-- Applies a function to all elements of the MSet.
+mapMSet :: (a -> b) -> MSet a -> MSet b
+mapMSet f (MS mset) = MS (map (applyFunc f) mset)
+  where
+    -- function to apply func to 1st elem of MSet type
+    applyFunc :: (a -> b) -> (a, Int) -> (b, Int)
+    applyFunc func (x, n) = (func x, n)
+
+-- Check if one MSet is a subset with the same multiplicity as another MSet
+strictSubeq :: Eq a => MSet a -> MSet a -> Bool
+strictSubeq (MS mset1) (MS mset2) = all checkElem mset1
+  where
+    -- function to check if an element in mset1 has the same multiplicity in mset2
+    checkElem :: (a, Int) -> Bool
+    checkElem (x, n) = occs (MS mset2) x == n
